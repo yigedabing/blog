b1c099c4bf72708c89da53be7dceb003796fc3d1
yigexiaobing <1459014701@qq.com>
2023-02-08 09:45:22 +0800

    build error fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,40 @@ import styles from "./index.module.css";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import "../css/styles.css";
 import "tailwindcss/tailwind.css";
-import { writeOneByOne } from "./utils";
+
+function writeOneByOne(text, dom, container, opt) {
+  const el = document.createElement(dom);
+  const bar = document.createElement("span");
+  bar.className = "write-vertical-bar";
+  el.appendChild(bar);
+  container.appendChild(el);
+
+  let timer = null;
+  let currentIndex = 0;
+  const sleep = opt.sleep || 0;
+
+  return new Promise((resolve) => {
+    function next() {
+      const textNode = document.createTextNode(text[currentIndex]);
+      el.insertBefore(textNode, bar);
+      currentIndex++;
+
+      if (currentIndex < text.length) {
+        timer = setTimeout(() => {
+          next();
+        }, 100);
+      } else {
+        timer = setTimeout(() => {
+          el.removeChild(bar);
+          clearTimeout(timer);
+          resolve(null);
+        }, sleep);
+      }
+    }
+
+    next();
+  });
+}
 
 export default class Home extends React.Component {
   state = {};
